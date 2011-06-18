52266037cb3e18afa1878abda3ec90c105c714e6
kuntoaji <kunto.aji.kr@gmail.com>
2011-06-18 17:25:31 +0700

    reformat the question

--- problem_00001.rb
+++ problem_00001.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 
 # http://projecteuler.net/index.php?section=problems&id=1
-# 05 October 2001
 #
 # Problem 1
+# 05 October 2001
 #
 # If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
 # The sum of these multiples is 23.
